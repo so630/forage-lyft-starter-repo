5a306c8f7d985bc293b0fc2599d2b8ac516b7290
Soham Chakraborty <sohamchakraborty@Sohams-MacBook-Pro.local>
2023-06-28 11:49:24 +0400

    separate the unit tests to three different files to test for batteries, tires and engines separately

--- tests/test_battery.py
+++ tests/test_battery.py
@@ -3,39 +3,7 @@ from datetime import datetime
 
 sys.path.append('../forage-lyft-starter-repo-fork')
 
-from models.Engine import CapuletEngine, WilloughbyEngine, SternmanEngine
 from models.Battery import NubbinBattery, SpindlerBattery
-
-
-class TestCapuletEngine(unittest.TestCase):
-    def test_should_be_serviced(self):
-        engine = CapuletEngine(last_service_mileage=30001, current_mileage=0)
-        self.assertTrue(engine.needs_service())
-
-    def test_should_not_be_serviced(self):
-        engine = CapuletEngine(last_service_mileage=3001, current_mileage=0)
-        self.assertFalse(engine.needs_service())
-
-class TestWilloughbyEngine(unittest.TestCase):
-    def test_should_be_serviced(self):
-
-        engine = WilloughbyEngine(last_service_mileage=60001, current_mileage=0)
-        self.assertTrue(engine.needs_service())
-
-    def test_should_not_be_serviced(self):
-        engine = WilloughbyEngine(last_service_mileage=5999, current_mileage=0)
-        self.assertFalse(engine.needs_service())
-
-class TestSternmanEngine(unittest.TestCase):
-    def test_should_be_serviced(self):
-        warning_light = True
-        engine = SternmanEngine(warning_light)
-        self.assertTrue(engine.needs_service())
-
-    def test_should_be_serviced(self):
-        warning_light = False
-        engine = SternmanEngine(warning_light)
-        self.assertFalse(engine.needs_service())
 
 class TestSpindlerBattery(unittest.TestCase):
     def test_should_be_serviced(self):
